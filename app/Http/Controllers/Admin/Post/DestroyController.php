ff26e7ea9774fe75c8ca595239fd4ecc422b4680
Dmitriy <vst-pcmusic@yandex.ru>
2021-12-04 01:57:18 +0300

    Test blog

--- app/Http/Controllers/Admin/Post/DestroyController.php
+++ app/Http/Controllers/Admin/Post/DestroyController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class DestroyController extends Controller
 {
     public function __invoke(Post $post)
     {
+        $post->tags()->sync([]);
+        Storage::disk('public')->delete($post->image);
         $post->delete();
 
         return redirect(route('admin.post.index'));
